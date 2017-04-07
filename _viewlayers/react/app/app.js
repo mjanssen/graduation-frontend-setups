4993ce4c602d1251bdc34af1e05b580bb3ba9782
M. Janssen <dev.marnix@gmail.com>
2017-04-07 12:03:38 +0200

    Implemented basic setup for react

---
+++ _viewlayers/react/app/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+// Define base component
+import App from './components/App/App';
+
+// Create init function to render application from
+function init() {
+  render(<App />, document.querySelector('#root'));
+}
+
+// While developing, enable HMR
+if (module.hot) {
+  module.hot.accept('./components/App/App', () => {
+    // requestAnimationFrame is used to do one paint within the browser
+    requestAnimationFrame(init);
+  });
+}
+
+init();
---
+++ _viewlayers/react/app/components/Paragraph/Paragraph.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// class Paragraph extends React.Component {
+//   render() {
+//     return (
+//       <p>How u doin?</p>
+//     );
+//   }
+// }
+const Paragraph = () => (
+  <p>How u doing?</p>
+);
+
+export default Paragraph;
