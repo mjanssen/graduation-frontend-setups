c0ab31cec965c90d363787101a47dbea66cf63b1
Marnix Janssen <marnixelmo@gmail.com>
2017-04-12 14:05:31 +0200

    Removed unused variables

--- _viewlayers/react-router/app/components/app/App.js
+++ _viewlayers/react-router/app/components/app/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import AppRouter from 'components/router/Router';
 
-const App = () => {
-  const title = 'React!!';
-  const path = '/components/App.js';
-  return (
-    <div>
-      <AppRouter />
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <AppRouter />
+  </div>
+);
 
 export default App;
